2f350d8db78445f79c9b14e014cbabaa87c37003
Slawa Gladkov <svyatoslav.gladkov@gmail.com>
2020-03-29 21:23:30 +0200

    Add initial list of two cards with icons

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,6 +1,28 @@
 <template>
-  <q-page class="flex flex-center">
-    <p>Text</p>
+  <q-page padding>
+    <div class="q-pb-md">
+      <q-list bordered>
+        <q-item clickable v-ripple>
+          <q-item-section avatar>
+            <q-icon color="primary" name="bluetooth" />
+          </q-item-section>
+
+          <q-item-section>Icon as avatar</q-item-section>
+        </q-item>
+      </q-list>
+    </div>
+
+    <div class="q-pb-md">
+      <q-list bordered>
+        <q-item clickable v-ripple>
+          <q-item-section avatar>
+            <q-icon color="primary" name="bluetooth" />
+          </q-item-section>
+
+          <q-item-section>Icon as avatar</q-item-section>
+        </q-item>
+      </q-list>
+    </div>
   </q-page>
 </template>
 
