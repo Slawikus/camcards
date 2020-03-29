a5a7e4636f4d7830c694737af873bfaed6223ab8
Slawa Gladkov <svyatoslav.gladkov@gmail.com>
2020-03-29 20:26:50 +0200

    Clean up

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,14 +1,32 @@
 <template>
   <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-full.svg"
-    >
+    <p>Text</p>
   </q-page>
 </template>
 
 <script>
+import Card from 'components/Card';
+
 export default {
-  name: 'PageIndex'
+  name: 'PageIndex',
+
+  components: {
+    Card
+  },
+
+  data () {
+    return {
+      cards: [
+        {
+          icon: 'Docs',
+          caption: 'quasar.dev'
+        },
+        {
+          icon: 'Docs',
+          caption: 'quasar.dev'
+        }
+      ]
+    }
+  }
 }
 </script>
