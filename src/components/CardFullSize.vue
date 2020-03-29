a31ac9cf03011b6e9a04700d7d5e9c3477356e95
Slawa Gladkov <svyatoslav.gladkov@gmail.com>
2020-03-30 01:24:30 +0200

    Fix the large icon and caption on the full size card to the center of the dialog modal

--- src/components/CardFullSize.vue
+++ src/components/CardFullSize.vue
@@ -4,13 +4,15 @@
       <q-btn icon="o_close" flat dense v-close-popup />
     </q-card-section>
 
-    <q-card-section>
-      <q-icon :name="iconPrefix + card.icon" class="full-width" size="24rem"/>
-    </q-card-section>
+    <div class="fixed-center">
+      <q-card-section>
+        <q-icon :name="iconPrefix + card.icon" class="full-width" size="24rem"/>
+      </q-card-section>
 
-    <q-card-section>
-      <div class="text-center text-uppercase text-h4">{{ card.caption }}</div>
-    </q-card-section>
+      <q-card-section>
+        <div class="text-center text-uppercase text-h4">{{ card.caption }}</div>
+      </q-card-section>
+    </div>
   </q-card>
 </template>
 
